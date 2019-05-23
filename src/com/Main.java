2fe0394d2e1fd8c4d8f6bea5b280c7b9aa9e61c0
pondzik <42366124+pondzik@users.noreply.github.com>
2019-05-23 16:23:30 +0200

    Add files via upload

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,13 +1,74 @@
-package com;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csvimport;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+/**
+ *
+ * @author student
+ */
+
+
+public class Csvimport {
+   
+    
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        //region DANE
-        int[] slots;
-        int[] days;
-        int[] leader;
-        int[][] defenses;
-        //endregion
+        
+        String csvFile = "/komisje.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] komisje = line.split(cvsSplitBy);
+                if (komisje.length !=3){
+                    
+                    System.out.println("zła ilość kolumn");
+                    System.exit(1);
+                                                        
+                }
+                else{
+                    
+                    
+                    
+                }
+               // System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        
+        // TODO code application logic here
     }
+    
 }
+
---
+++ src/com/Obrony.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csvimport;
+
+/**
+ *
+ * @author student
+ */
+public class Obrony {
+    int id_rec,id_prom;
+    public Obrony(){}
+    public Obrony(String idr, String idp){
+    id_rec = Integer.parseInt(idr);
+    id_prom = Integer.parseInt(idp);
+    
+    }
+    
+    
+}
