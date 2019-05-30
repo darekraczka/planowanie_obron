05615b95438998185f5c75705018d0c5800f0b7d
Dariusz Rączka <d.raczka96@gmail.com>
2019-05-30 14:16:10 +0200

    Package change

--- src/com/Projekt2.java
+++ src/com/Projekt2.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projekt2;
+package com;
 import ilog.concert.*;
 import ilog.cp.IloCP;
 import java.util.ArrayList;
