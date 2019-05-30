76e16181f98b7b12b2b94744e184924cbd7891ad
unknown <aleksandramusial1997@gmail.com>
2019-05-30 14:44:57 +0200

    1

--- src/com/main.java
+++
@@ -1,9 +0,0 @@
-package com;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Problem problem = new Problem("komisje.csv","obrony.csv");
-        System.out.println();
-    }
-}
