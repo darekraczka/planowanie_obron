09f367f8856ff8735c2110fa039b5f893fca9003
Dariusz Rączka <d.raczka96@gmail.com>
2019-06-13 15:02:59 +0200

    Update MainTest.java

--- src/com/MainTest.java
+++ src/com/MainTest.java
@@ -6,8 +6,7 @@ import java.util.List;
 public class MainTest {
 
     public static void main(String[] args) {
-        List<ExcelData> excelDataList = Reader.readDataFromExcel("obrony_2017.xlsx");
-        Problem problem = Reader.createProblem(excelDataList);
-        System.out.println();
+        Problem problem = Reader.createProblem(Reader.readDataFromExcel("obrony_2017.xlsx"));
+        System.out.println();"obrony_2017.xlsx"
     }
 }
